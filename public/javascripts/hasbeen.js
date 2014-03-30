b4c2ca5f1bc5ddc52cebd247295e90ce083320f9
Klaus Meyer <spam@klaus-meyer.net>
2014-03-30 16:58:50 +0200

    show all locations of a traveller on one map

--- public/javascripts/hasbeen.js
+++ public/javascripts/hasbeen.js
@@ -1,23 +1,48 @@
-/**
- * I've always wanted to define that function!
- */
-function goto(location) {
-  if(location) {
-    location = decodeURIComponent(location);
+var locations = [];
 
-    var geocoder        = new google.maps.Geocoder();
-    var geocoderRequest = { address: location }
-
-    geocoder.geocode(geocoderRequest, function(results, status) {
+function map() {
+  var bounds = new google.maps.LatLngBounds();
+  var geocoder = new google.maps.Geocoder();
+  var $bar_container = $('#bar');
+  var $map_container = $('#map');
+  $map_container.css('margin-top', $bar_container.height());
+  var map = new google.maps.Map($map_container.get(0), { disableDefaultUI: true });
+  function setup_locations() {
+    var single_location = locations.pop();
+    geocoder.geocode({ address: single_location }, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
-        var mapOptions = {
-          disableDefaultUI: true,
-          mapTypeId:        google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        var marker = new google.maps.Marker({
+          position: results[0].geometry.location,
+          map: map,
+          title: single_location
+        });
+        bounds.extend(marker.getPosition());
+      }
+    });
+    locations.length ? window.setTimeout(setup_locations, 100) : map.fitBounds(bounds)
+  }
+  if(locations.length == 1) {
+    geocoder.geocode({ address: locations.pop() }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
         map.fitBounds(results[0].geometry.viewport);
         map.setCenter(results[0].geometry.location);
       }
     });
   }
+  else {
+    google.maps.event.addDomListener(window, 'load', setup_locations);
+  }
+}
+
+/**
+ * I've always wanted to define that function!
+ */
+function goto(single_location) {
+  locations = [decodeURIComponent(single_location)];
+  map();
+}
+
+function overview(location_list) {
+  locations = location_list;
+  map();
 }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/rspec'
+require 'json'
 
 Capybara.configure do |config|
   config.app = Padrino.application

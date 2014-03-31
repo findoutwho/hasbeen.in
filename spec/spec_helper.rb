93ea2b9d7ffeada22583776a8366cd3f2b29b6d7
Klaus Meyer <spam@klaus-meyer.net>
2014-03-31 20:02:17 +0200

    add json to gemfile and remove explicit require in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/rspec'
-require 'json'
 
 Capybara.configure do |config|
   config.app = Padrino.application
